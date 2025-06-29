b3c002222d5c20f6f07a01e15f4657fc2f308f71
PearlHanh <hanhhiennn15@gmail.com>
2025-06-29 18:57:57 +0700

    Deploy lên Netlify - chỉnh sửa basename và cấu hình build

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./CSS/Login.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter basename="/ProjectII">
+      <BrowserRouter>
          <App />
       </BrowserRouter>  
       </React.StrictMode>
