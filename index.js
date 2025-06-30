fcc9b262da9cf5b4000d1ddb1decc1e1ffc52235
PearlHanh <hanhhiennn15@gmail.com>
2025-06-30 23:25:01 +0700

    Repair CORS

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://hoanganhbui2110.netlify.app");
   res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
   next();
 });
 // Kết nối đến cùng 1 DATABASE_URL nhưng phân biệt schema bằng search_path
