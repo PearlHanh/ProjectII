30976b91427fa84d10f6e4e8cf5d6e8568023d75
PearlHanh <hanhhiennn15@gmail.com>
2025-06-30 23:40:55 +0700

    cors

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
+app.options('*', (req, res) => {
+  res.set({
+    'Access-Control-Allow-Origin': 'https://hoanganhbui2110.netlify.app',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  });
+  res.sendStatus(200);
+});
 app.use((req, res, next) => {
   console.log(`[${req.method}] ${req.originalUrl}`);
   next();
