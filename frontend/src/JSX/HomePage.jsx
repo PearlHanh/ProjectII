becce76f930bf2331efe83d02e4872fde7993c52
PearlHanh <hanhhiennn15@gmail.com>
2025-07-02 12:40:58 +0700

    123

--- frontend/src/JSX/HomePage.jsx
+++ frontend/src/JSX/HomePage.jsx
@@ -316,8 +316,7 @@ const handleUpdateClick = (employee) => {
 {activedId === 3 && (
   <div className="timesheettable">
     <div className="table3">
-      <div className="timekeeping-container">
-        <h1>Danh sách nhân viên</h1>
+    
                 {/* ✅ Layout chia 2 cột: bảng + tab */}
         <div className="employee-layout">
           {/* Bảng nhân viên */}
@@ -397,7 +396,6 @@ const handleUpdateClick = (employee) => {
         </div>
       </div>
     </div>
-  </div>
 )}
             {activedId === 4 && <div className="historytable">
             <div className="table4">
