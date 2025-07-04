272c1e8f4a917845f4d27aec8384e6cc7030b223
PearlHanh <hanhhiennn15@gmail.com>
2025-07-05 06:31:50 +0700

    123

--- frontend/src/JSX/App.jsx
+++ frontend/src/JSX/App.jsx
@@ -17,6 +17,7 @@ export default function App() {
             </PrivateRoute>
           }
         />
+        <Route path="/login" element={<Login />}/>         
         <Route path="/order/:tableID" element={<Order />} />
         <Route path='/order/:tableID/cart' element={<Cart />}  />
   </Routes>
